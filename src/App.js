e9b2df48ad25113ef69c3d629f04bf93142b6885
spotfreeman <r.oyarzun83@gmail.com>
2025-05-25 01:09:44 -0400

    Add Page TempData

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import DatosPage from './DatosPage';
+import { TempData } from './Pages/TempData';
 
 
 function App() {
@@ -9,10 +10,14 @@ function App() {
       <nav>
         <Link to="/">Inicio</Link>
         <Link to="/datos">Datos</Link>
+        <Link to="/tempdata">TempData</Link>
       </nav>
+
       <Routes>
         <Route path="/" element={<div>Página principal</div>} />
         <Route path="/datos" element={<DatosPage />} />
+        <Route path="/tempdata" element={<TempData />} />
+        <Route path="*" element={<div>404 - Página no encontrada</div>} />
       </Routes>
     </Router>
   );
---
+++ src/Pages/TempData.jsx
@@ -0,0 +1,7 @@
+
+
+export const TempData = () => {
+    return (
+        <div>TempData</div>
+    )
+}
