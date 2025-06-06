ea7be729d4531ea27f23c26816d18b0e443d5a42
spotfreeman <r.oyarzun83@gmail.com>
2025-06-05 22:27:29 -0400

    Implementando Modulo Proyectos

--- src/Pages/proyectos/Lista.jsx
+++ src/Pages/proyectos/Lista.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-const Lista = () => {
+export const Lista = () => {
     const [proyectos, setProyectos] = useState([]);
 
     useEffect(() => {
