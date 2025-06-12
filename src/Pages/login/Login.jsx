f382fa4e53970afcb87298bc8bf0bdf88f304178
Raúl Oyarzún Becerra <r.oyarzun83@gmail.com>
2025-06-12 15:07:39 -0400

    Mejoras : New Login System

--- src/Pages/login/Login.jsx
+++ src/Pages/login/Login.jsx
@@ -18,8 +18,8 @@ export const Login = () => {
                                 src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
                                 className="h-10 w-auto"
                             />
-                            <h2 className="mt-8 text-2xl/9 font-bold tracking-tight text-gray-900">Inicio de Sesion</h2>
-                            <p className="mt-2 text-sm/6 text-gray-500">
+                            <h2 className="mt-8 text-2xl font-bold tracking-tight text-gray-900">Inicio de Sesion</h2>
+                            <p className="mt-2 text-sm text-gray-500">
                                 Not a member?{' '}
                                 <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
                                     Start a 14 day free trial
@@ -31,7 +31,7 @@ export const Login = () => {
                             <div>
                                 <form action="#" method="POST" className="space-y-6">
                                     <div>
-                                        <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                                        <label htmlFor="email" className="block text-sm font-medium text-gray-900">
                                             Email address
                                         </label>
                                         <div className="mt-2">
@@ -41,7 +41,7 @@ export const Login = () => {
                                                 type="email"
                                                 required
                                                 autoComplete="email"
-                                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600 sm:text-sm"
                                             />
                                         </div>
                                     </div>
@@ -109,7 +109,7 @@ export const Login = () => {
                                     <div>
                                         <button
                                             type="submit"
-                                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                         >
                                             Sign in
                                         </button>
