ed6222b0ae0ceacd92b74454e2f2ba47627159e2
spotfreeman <r.oyarzun83@gmail.com>
2025-06-21 10:04:11 -0400

    Tailwind Testing Components

--- src/Pages/testing/Sidebar2.jsx
+++ src/Pages/testing/Sidebar2.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import { CalendarIcon, ChartPieIcon, DocumentDuplicateIcon, FolderIcon, HomeIcon, UsersIcon } from '@heroicons/react/24/outline';
 
-const navigation = [
-    { name: 'Dashboard', href: '#', icon: HomeIcon, count: '5', current: true },
-    { name: 'Team', href: '#', icon: UsersIcon, current: false },
-    { name: 'Projects', href: '#', icon: FolderIcon, count: '12', current: false },
-    { name: 'Calendar', href: '#', icon: CalendarIcon, count: '20+', current: false },
-    { name: 'Documents', href: '#', icon: DocumentDuplicateIcon, current: false },
-    { name: 'Reports', href: '#', icon: ChartPieIcon, current: false },
-]
-const teams = [
-    { id: 1, name: 'Heroicons', href: '#', initial: 'H', current: false },
-    { id: 2, name: 'Tailwind Labs', href: '#', initial: 'T', current: false },
-    { id: 3, name: 'Workcation', href: '#', initial: 'W', current: false },
-]
-
-function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
-}
 
 export const Sidebar2 = () => {
+
+    const navigation = [
+        { name: 'Dashboard', href: '#', icon: HomeIcon, count: '5', current: true },
+        { name: 'Team', href: '#', icon: UsersIcon, current: false },
+        { name: 'Projects', href: '#', icon: FolderIcon, count: '12', current: false },
+        { name: 'Calendar', href: '#', icon: CalendarIcon, count: '20+', current: false },
+        { name: 'Documents', href: '#', icon: DocumentDuplicateIcon, current: false },
+        { name: 'Reports', href: '#', icon: ChartPieIcon, current: false },
+    ]
+    const teams = [
+        { id: 1, name: 'Heroicons', href: '#', initial: 'H', current: false },
+        { id: 2, name: 'Tailwind Labs', href: '#', initial: 'T', current: false },
+        { id: 3, name: 'Workcation', href: '#', initial: 'W', current: false },
+    ]
+
+    function classNames(...classes) {
+        return classes.filter(Boolean).join(' ')
+    }
+
     return (
         <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6">
             <div className="flex h-16 shrink-0 items-center">
