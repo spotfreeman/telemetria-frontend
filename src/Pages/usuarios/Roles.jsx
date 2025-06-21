febb6ad5b3075210903209143461735ad3c5b0dd
spotfreeman <r.oyarzun83@gmail.com>
2025-06-21 08:44:56 -0400

    Tailwind problemas

--- src/Pages/usuarios/Roles.jsx
+++ src/Pages/usuarios/Roles.jsx
@@ -24,10 +24,26 @@ export const Roles = () => {
                                 {rol === "Administrador" && (
                                     <div>
                                         {/* Contenido solo para administradores */}
-                                        Validacion eres Administrador.
                                         <span className="inline-flex items-center rounded-full bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-gray-500/10 ring-inset">
-                                            Badge
+                                            Administrador
                                         </span>
+                                        <p>otro objeto</p>
+                                        <div>
+                                            <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                                                Email
+                                            </label>
+                                            <div className="mt-2">
+                                                <input
+                                                    defaultValue="you@example.com"
+                                                    id="email"
+                                                    name="email"
+                                                    type="email"
+                                                    disabled
+                                                    placeholder="you@example.com"
+                                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:outline-gray-200 sm:text-sm/6"
+                                                />
+                                            </div>
+                                        </div>
                                     </div>
 
                                 )}
