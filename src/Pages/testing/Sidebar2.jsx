faaa8e1ae172131bc8bbf52cbcdd7a95256ee67e
spotfreeman <r.oyarzun83@gmail.com>
2025-06-21 09:58:01 -0400

    Tailwind Testing Components

--- src/Pages/testing/Sidebar2.jsx
+++ src/Pages/testing/Sidebar2.jsx
@@ -1,13 +1,5 @@
 import React from 'react'
-
-import {
-    CalendarIcon,
-    ChartPieIcon,
-    DocumentDuplicateIcon,
-    FolderIcon,
-    HomeIcon,
-    UsersIcon,
-} from '@heroicons/react/24/outline'
+import { CalendarIcon, ChartPieIcon, DocumentDuplicateIcon, FolderIcon, HomeIcon, UsersIcon } from '@heroicons/react/24/outline';
 
 const navigation = [
     { name: 'Dashboard', href: '#', icon: HomeIcon, count: '5', current: true },
