e6b070e5b847ad95fde560351adf862de2f2c35d
spotfreeman <r.oyarzun83@gmail.com>
2025-06-22 13:43:20 -0400

    ProyectoDetalle : Fechas y Calculos

--- src/Pages/proyectos/ProyectoDetalle.jsx
+++ src/Pages/proyectos/ProyectoDetalle.jsx
@@ -443,6 +443,7 @@ export const ProyectoDetalle = () => {
                     <tbody>
                         {proyecto.fechas?.map((fecha, idx) => (
                             <tr key={idx}>
+                                <h3>{fecha.fechainicio}</h3>
                                 <td className="border"> {fecha.fechainicio}
                                     {fecha.fechainicio
                                         ? parseFecha(fecha.fechainicio).toLocaleDateString('es-CL')
