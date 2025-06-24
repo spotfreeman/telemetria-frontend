6d7892c024c23f9329455f1757c555920c58e028
Raúl Oyarzún Becerra <r.oyarzun83@gmail.com>
2025-06-24 14:26:07 -0400

    Server : ejemplo con imagenes

--- src/Pages/Server.jsx
+++ src/Pages/Server.jsx
@@ -15,28 +15,6 @@ export const Server = () => {
 
     const ultimaIpExterna = datos.length > 0 ? datos[0].ip_externa : "No disponible";
 
-    const people = [
-        {
-            name: 'Servidor  7 Days',
-            role: 'Senior Developer',
-            imageUrl:
-                'https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-            bio: 'Praesentium iure error aliquam voluptas ut libero. Commodi placeat sit iure nulla officiis. Ut ex sit repellat tempora. Qui est accusamus exercitationem natus ut voluptas. Officiis velit eos ducimus.',
-            xUrl: '#',
-            linkedinUrl: '#',
-        },
-        {
-            name: 'Servidor Unity',
-            role: 'Senior Developer',
-            imageUrl:
-                'https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-            bio: 'Praesentium iure error aliquam voluptas ut libero. Commodi placeat sit iure nulla officiis. Ut ex sit repellat tempora. Qui est accusamus exercitationem natus ut voluptas. Officiis velit eos ducimus.',
-            xUrl: '#',
-            linkedinUrl: '#',
-        },
-    ]
-
-
     const servidores = [
         {
             name: 'Servidor 7 Days',
@@ -58,6 +36,21 @@ export const Server = () => {
 
     return (
         <div className="flex flex-col items-center mt-8">
+
+            <div className="w-full bg-gray-200 py-3 px-2 flex items-center justify-between shadow mb-8">
+                {/* Título a la izquierda */}
+                <h1 className="text-black text-xl font-bold flex-1">Notas</h1>
+
+                {/* Botón Agregar Nota al centro */}
+                <div className="flex-1 flex justify-center">
+                    <h3>centro</h3>
+                </div>
+                {/* Botón Exportar a Excel a la derecha */}
+                <div className="flex-1 flex justify-end">
+                    <h3>derecha</h3>
+                </div>
+            </div>
+
             <div>
 
                 <h2 className="text-2xl font-bold mb-4">Datos de Raspberry Pi </h2>
@@ -65,53 +58,7 @@ export const Server = () => {
                 <h3 className="text-xl mb-2 font-bold">IP Servidor Unity : {ultimaIpExterna}</h3>
             </div>
 
-            <div className="bg-white py-24 md:py-32 lg:py-40">
-                <div className="mx-auto grid max-w-7xl grid-cols-1 gap-20 px-6 lg:px-8 xl:grid-cols-3">
-                    <div className="mx-auto max-w-2xl lg:mx-0">
-                        <h2 className="text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Servidores</h2>
-                        <p className="mt-6 text-lg/8 text-gray-600">
-                            Servidores disponibles para juegos y aplicaciones, optimizados para ofrecer la mejor experiencia de usuario.
-                        </p>
-                    </div>
-                    <ul
-                        role="list"
-                        className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-20 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:gap-x-8 xl:col-span-2"
-                    >
-                        {people.map((person) => (
-                            <li key={person.name}>
-                                <img alt="" src={person.imageUrl} className="aspect-[3/2] w-full rounded-2xl object-cover" />
-                                <h3 className="mt-6 text-lg/8 font-semibold text-gray-900">{person.name}</h3>
-                                <p className="text-base/7 text-gray-600">{person.role}</p>
-                                <p className="mt-4 text-base/7 text-gray-600">{person.bio}</p>
-                                <ul role="list" className="mt-6 flex gap-x-6">
-                                    <li>
-                                        <a href={person.xUrl} className="text-gray-400 hover:text-gray-500">
-                                            <span className="sr-only">X</span>
-                                            <svg fill="currentColor" viewBox="0 0 20 20" aria-hidden="true" className="size-5">
-                                                <path d="M11.4678 8.77491L17.2961 2H15.915L10.8543 7.88256L6.81232 2H2.15039L8.26263 10.8955L2.15039 18H3.53159L8.87581 11.7878L13.1444 18H17.8063L11.4675 8.77491H11.4678ZM9.57608 10.9738L8.95678 10.0881L4.02925 3.03974H6.15068L10.1273 8.72795L10.7466 9.61374L15.9156 17.0075H13.7942L9.57608 10.9742V10.9738Z" />
-                                            </svg>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href={person.linkedinUrl} className="text-gray-400 hover:text-gray-500">
-                                            <span className="sr-only">LinkedIn</span>
-                                            <svg fill="currentColor" viewBox="0 0 20 20" aria-hidden="true" className="size-5">
-                                                <path
-                                                    d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
-                                                    clipRule="evenodd"
-                                                    fillRule="evenodd"
-                                                />
-                                            </svg>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
-
-
+            {/* Lista de Servidores */}
             <div className="bg-white py-24 md:py-32 lg:py-40">
                 <div className="mx-auto grid max-w-7xl grid-cols-1 gap-20 px-6 lg:px-8 xl:grid-cols-3">
                     <div className="mx-auto max-w-2xl lg:mx-0">
