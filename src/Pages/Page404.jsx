9998177a32c90d4a52f28d53195246a8e5cead2d
spotfreeman <r.oyarzun83@gmail.com>
2025-09-10 23:41:06 -0300

    Modificando sistema de usuarios

--- src/Pages/Page404.jsx
+++ src/Pages/Page404.jsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 
 export const Page404 = () => {
     return (
-<<<<<<< HEAD
         <main className="relative isolate min-h-full">
             <img
                 alt=""
@@ -25,28 +24,6 @@ export const Page404 = () => {
                     >
                         <span aria-hidden="true">&larr;</span> Back to home
                     </Link>
-=======
-        <>
-            <main className="relative isolate min-h-full">
-                <img
-                    alt=""
-                    src="https://images.unsplash.com/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75"
-                    className="absolute inset-0 -z-10 size-full object-cover object-top"
-                />
-                <div className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
-                    <p className="text-base/8 font-semibold text-white">404</p>
-                    <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">
-                        Page not found
-                    </h1>
-                    <p className="mt-6 text-lg font-medium text-pretty text-white/70 sm:text-xl/8">
-                        Sorry, we couldn’t find the page you’re looking for.
-                    </p>
-                    <div className="mt-10 flex justify-center">
-                        <a href="#" className="text-sm/7 font-semibold text-white">
-                            <span aria-hidden="true">&larr;</span> Back to home
-                        </a>
-                    </div>
->>>>>>> 5979877aed9903a4976f9fa3d4f6d3c7d43609de
                 </div>
             </div>
         </main>
